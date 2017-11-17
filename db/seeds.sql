b6fc18ab0b453393dc563306b380ed2ae8688e39
davevish <vishvesh01@yahoo.com>
2017-11-17 15:00:30 -0500

    starting files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (1, 'veggie burger', false);
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (2, 'cheese burger', false);
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (3, 'grilled chicken breast burger', false);
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (4, 'all american burger', false);
